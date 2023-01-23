2268c384888164e46bb615176e6c0997be2a89c1
V1ntage <marcos.vinicius.r.alencar@gmail.com>
2023-01-22 23:08:33 -0300

    Menu

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MenuApp />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MenuApp from './components/MenuApp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MenuApp
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0px;
+  padding: 0px;
+}
+@font-face {
+  font-family: GoodTimes-Regular;
+  src: url(./assets/fonts/goodTimesRegular.otf);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
+  
+}
+body{
+  background-color: #29303a
 }
 </style>
---
+++ src/components/MenuApp.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="menu">
+    <div class="item" href="">
+      <img class="item-icon" src="../assets/img/home.svg" alt="" srcset="" />
+    </div>
+    <div id="menu-title" class="item" href="">WAIFU LEARN</div>
+    <div class="item" href="">
+      <img class="item-icon" src="../assets/img/options.svg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuApp",
+  props: {},
+};
+</script>
+
+<style scoped>
+.menu {
+  background-color: rgb(41, 40, 41);
+  position: fixed;
+  bottom: 0px;
+  height: 70px;
+  width: 100%;
+  -webkit-box-shadow: -13px 46px 93px 30px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: -13px 46px 93px 30px rgba(0, 0, 0, 0.75);
+  box-shadow: -13px 46px 93px 30px rgba(0, 0, 0, 0.75);
+}
+
+.item {
+  width: calc(98% / 3);
+  color: #fff;
+  font-size: 22px;
+  display: inline-block;
+  cursor: pointer;
+  transition: 0.5s;
+}
+.item:hover{
+  opacity: 0.2;
+}
+.item-icon {
+  margin: 20px auto 0px auto;
+}
+#menu-title{
+  font-family: GoodTimes-Regular;
+  margin-bottom: 10px;
+}
+</style>
